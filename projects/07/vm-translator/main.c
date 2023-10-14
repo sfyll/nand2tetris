5f6aa3b95546811527f1d731ffb04603b8b652f1
SFYLL <santiagoflood@hotmail.fr>
2023-10-14 12:24:51 +0200

    Better handling of Instruction

--- projects/07/vm-translator/main.c
+++ projects/07/vm-translator/main.c
@@ -2,51 +2,131 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef enum {
+    C_INSTRUCTION,
+    C_POP,
+    C_PUSH,
+    INVALID_TYPE
+} CommandType;
+
+typedef struct {
+  CommandType commandType;
+  char *arg1;
+  int arg2;
+} VMInstruction;
+
+typedef struct {
+  char *command;
+  CommandType type;
+} CommandMap;
+
+CommandMap commands[] = {
+    {"add", C_INSTRUCTION}, {"sub", C_INSTRUCTION}, {"neg", C_INSTRUCTION},
+    {"eq", C_INSTRUCTION},  {"gt", C_INSTRUCTION},  {"lt", C_INSTRUCTION},
+    {"and", C_INSTRUCTION}, {"or", C_INSTRUCTION},  {"not", C_INSTRUCTION},
+    {"push", C_PUSH},       {"pop", C_POP},
+};
+
+//The below is for easy printing and debugging
+const char *CommandTypeStrings[] = {
+    "C_INSTRUCTION",
+    "C_POP",
+    "C_PUSH",
+    "INVALID_TYPE"
+};
+
+//Sentinel values from not yet supported conmmands
+static const CommandMap INVALID_COMMAND = {NULL, INVALID_TYPE};
+
+
+CommandMap getCommandType(const char *command) {
+  for (int i = 0; i < sizeof(commands) / sizeof(CommandMap); i++) {
+    if (strcmp(commands[i].command, command) == 0) {
+      return commands[i];
+    }
+  }
+  return INVALID_COMMAND; // Not found
+}
+
+void printInstructions(VMInstruction *instructions, int lineCount) {
+    for (int i = 0; i < lineCount; i++) {
+        printf("Instruction %d:\n", i + 1);
+        printf("\tCommand Type: %s\n", CommandTypeStrings[instructions[i].commandType]);
+        
+        if (instructions[i].commandType == C_INSTRUCTION) {
+            printf("\tArg1: %s\n", instructions[i].arg1 ? instructions[i].arg1 : "N/A");
+            printf("\tArg2: N/A\n");
+        } else if ((instructions[i].commandType == C_PUSH) ||
+            (instructions[i].commandType == C_POP)) {
+            printf("\tArg1: %s\n", instructions[i].arg1 ? instructions[i].arg1 : "N/A");
+            printf("\tArg2: %d\n", instructions[i].arg2);
+        }
+    }
+}
+
 #define MAX_LINE_SIZE 10000
 
-char ***loadFile(const char *filePath, int *lineCount) {
-    FILE *file = fopen(filePath, "r");
-    if (file == NULL) {
-        perror("Error opening file");
-        return NULL;
+//We returns instructions, a pointer to an array of VMInstruction
+VMInstruction *loadFile(const char *filePath, int *lineCount) {
+  FILE *file = fopen(filePath, "r");
+  if (file == NULL) {
+    perror("Error opening file");
+    return NULL;
+  }
+
+  VMInstruction *instructions = NULL;
+  char buffer[MAX_LINE_SIZE];
+
+  while (fgets(buffer, sizeof(buffer), file) != NULL) {
+    char *token;
+    int wordCount = 0;
+
+    token = strtok(buffer, " \t\r\n");
+
+    // Ignore empty or comment lines
+    if (!token || token[0] == '/')
+      continue;
+
+    CommandMap commandType = getCommandType(token);
+    if (commandType.type == INVALID_COMMAND.type) {
+      fprintf(stderr, "Error: Unrecognized command in line: %d\n", *lineCount);
+      fprintf(stderr, "Line in question: %s\n", buffer);
+      fprintf(stderr, "Token in question: %s\n", token);
+      exit(EXIT_FAILURE);
     }
 
-    char ***lines = NULL; // List of list of words
-    char buffer[MAX_LINE_SIZE];
-    *lineCount = 0;
+    VMInstruction currentInstr;
+    currentInstr.commandType = commandType.type;
 
-    while (fgets(buffer, sizeof(buffer), file) != NULL) {
-        // Resize lines to accommodate the new lne
-        lines = realloc(lines, (*lineCount + 1) * sizeof(char **));
-        lines[*lineCount] = NULL;  // Initialize to NULL for later use with realloc
-
-        char *token;
-        int wordCount = 0;
-
-        token = strtok(buffer, " \n");
-        while (token != NULL) {
-            // Resize the current line to accommodate the new word
-            lines[*lineCount] = realloc(lines[*lineCount], (wordCount + 1) * sizeof(char *));
-            lines[*lineCount][wordCount] = strdup(token);
-            wordCount++;
-            token = strtok(NULL, " \n");
-        }
-
-        (*lineCount)++;
+    if (currentInstr.commandType == C_INSTRUCTION) {
+      currentInstr.arg1 = commandType.command;
+      currentInstr.arg2 = 0;
+    } else if ((currentInstr.commandType == C_PUSH) ||
+               (currentInstr.commandType == C_POP)) {
+      currentInstr.arg1 = commandType.command;
+          strtok(NULL, " \n"); // Get the next token for arg1
+      currentInstr.arg2 = atoi(strtok(
+          NULL, " \n")); // Get the next token for arg2 and convert to int
     }
+    instructions =
+        realloc(instructions, (*lineCount + 1) * sizeof(VMInstruction));
+    instructions[*lineCount] = currentInstr;
+    (*lineCount)++;
+  }
 
-    fclose(file);
-    return lines;
+  fclose(file);
+  return instructions;
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Usage: %s <file_path>\n", argv[0]);
-        return 1;
-    }
+  if (argc != 2) {
+    printf("Usage: %s <file_path>\n", argv[0]);
+    return 1;
+  }
 
-    int lineCount = 0;
-    char ***lines = loadFile(argv[1], &lineCount);
+  int lineCount = 0;
+  VMInstruction *instructions = loadFile(argv[1], &lineCount);
 
-    return 0;
+  printInstructions(instructions, lineCount);
+  return 0;
 }
