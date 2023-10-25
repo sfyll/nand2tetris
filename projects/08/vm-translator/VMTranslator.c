ebb549471959db03d8a88968d04c141b457277b3
SFYLL <santiagoflood@hotmail.fr>
2023-10-25 10:24:39 +0200

    Final VM Translator

--- projects/08/vm-translator/VMTranslator.c
+++ projects/08/vm-translator/VMTranslator.c
@@ -13,52 +13,54 @@
 
 void executeInstructions(VMInstruction *instructions, int lineCount,
                          HackMemory *memory, FileData outputFile) {
+  static char lastFunctionName[255] = {
+      0}; // Assuming function name won't exceed 255 chars
+  static int functionCallCounter = 0;
   for (int i = 0; i < lineCount; i++) {
     VMInstruction instr = instructions[i];
-    printf("RIGHT HERE: %d, %s, %d\n", instr.commandType, instr.arg1, instr.arg2);
     const char *assembly_code = getAssemblyTemplate(&instr);
 
     switch (instr.commandType) {
     case C_PUSH:
-      pushToStack(memory, instr.arg1, instr.arg2);
+      // pushToStack(memory, instr.arg1, instr.arg2);
       writePushAssembly(assembly_code, instr.arg1, &instr.arg2, outputFile);
       break;
 
     case C_POP:
-      popFromStack(memory, instr.arg1, instr.arg2);
+      // popFromStack(memory, instr.arg1, instr.arg2);
       writePopAssembly(assembly_code, instr.arg1, &instr.arg2, outputFile);
       break;
 
     case C_INSTRUCTION:
       if (strcmp(instr.arg1, "add") == 0) {
-        hack_add(memory);
+        // hack_add(memory);
         writeAddAssembly(assembly_code, outputFile);
       } else if (strcmp(instr.arg1, "sub") == 0) {
-        hack_sub(memory);
+        // hack_sub(memory);
         writeSubAssembly(assembly_code, outputFile);
       } else if (strcmp(instr.arg1, "neg") == 0) {
-        hack_neg(memory);
+        // hack_neg(memory);
         writeNegAssembly(assembly_code, outputFile);
       } else if (strcmp(instr.arg1, "eq") == 0) {
-        hack_eq(memory);
+        // hack_eq(memory);
         short value_to_pass = (short)i;
         writeEqAssembly(assembly_code, &value_to_pass, outputFile);
       } else if (strcmp(instr.arg1, "gt") == 0) {
-        hack_gt(memory);
+        // hack_gt(memory);
         short value_to_pass = (short)i;
         writeGtAssembly(assembly_code, &value_to_pass, outputFile);
       } else if (strcmp(instr.arg1, "lt") == 0) {
-        hack_lt(memory);
+        // hack_lt(memory);
         short value_to_pass = (short)i;
         writeLtAssembly(assembly_code, &value_to_pass, outputFile);
       } else if (strcmp(instr.arg1, "and") == 0) {
-        hack_and(memory);
+        // hack_and(memory);
         writeAndAssembly(assembly_code, outputFile);
       } else if (strcmp(instr.arg1, "or") == 0) {
-        hack_or(memory);
+        // hack_or(memory);
         writeOrAssembly(assembly_code, outputFile);
       } else if (strcmp(instr.arg1, "not") == 0) {
-        hack_not(memory);
+        // hack_not(memory);
         writeNotAssembly(assembly_code, outputFile);
       }
       break;
@@ -75,9 +77,18 @@ void executeInstructions(VMInstruction *instructions, int lineCount,
 
     case FUNCTION:
       if (strcmp(instr.arg1, "function") == 0) {
-        writeFunctionLabelAssembly(assembly_code, instr.arg3, instr.arg2, outputFile);
+        writeFunctionLabelAssembly(assembly_code, instr.arg3, instr.arg2,
+                                   outputFile);
       } else if (strcmp(instr.arg1, "return") == 0) {
         writeFunctionReturnAssembly(assembly_code, outputFile);
+      } else if (strcmp(instr.arg1, "call") == 0) {
+        if (strcmp(instr.arg3, lastFunctionName) == 0) {
+          functionCallCounter++;
+        } else {
+          strncpy(lastFunctionName, instr.arg3, sizeof(lastFunctionName));
+          functionCallCounter = 0;
+        }
+        writeFunctionCallAssembly(assembly_code, instr.arg3, instr.arg2, functionCallCounter, outputFile);
       }
       break;
 
@@ -85,25 +96,14 @@ void executeInstructions(VMInstruction *instructions, int lineCount,
       printf("Unknown instruction type.\n");
       break;
     }
-    printHackMemory(*memory);
+    // printHackMemory(*memory);
   }
-  writeEndOfFileAssembly(outputFile);
+  //writeEndOfFileAssembly(outputFile);
 }
 
-void processFile(const char *filePath, FileData file) {
+void processFile(const char *filePath, FileData file, HackMemory memory) {
   int lineCount = 0;
-  printf("%s\n", filePath);
-  VMInstruction *instructions = loadFile(filePath, &lineCount);
-  printf("UP\n");
-  
-  // Create memory component
-  HackMemory memory = initHackMemory();
-
-  printf("UP\n");
-  // Initialize assembly templates
-  initializeTemplates();
-  // Populate Instruction mapper to assembly template
-  initializeAssemblyMappings();
+  VMInstruction *instructions = loadFile(filePath, &lineCount, &file);
 
   executeInstructions(instructions, lineCount, &memory, file);
 }
@@ -116,12 +116,25 @@ int main(int argc, char *argv[]) {
 
   struct stat path_stat;
   stat(argv[1], &path_stat);
-
   FileData file;
   char outputPath[256] = {0};
 
+  // Create memory component
+  HackMemory memory = initHackMemory();
+
+  // Initialize assembly templates
+  initializeTemplates();
+  // Populate Instruction mapper to assembly template
+  initializeAssemblyMappings();
+  
   if (S_ISDIR(path_stat.st_mode)) {
-    snprintf(outputPath, sizeof(outputPath), "%s.vm", basename(argv[1]));
+          char dirPart[256];
+    char basePart[256];
+    char baseFolder[256];
+    strncpy(dirPart, argv[1], sizeof(dirPart));
+    strncpy(basePart, argv[1], sizeof(basePart));
+    strncpy(baseFolder, argv[1], sizeof(baseFolder));
+    snprintf(outputPath, sizeof(outputPath), "%s/%s/%s.vm", dirname(dirPart), basename(baseFolder), basename(basePart));
     file = openOutputFile(outputPath);
 
     if (!file.file) {
@@ -135,17 +148,16 @@ int main(int argc, char *argv[]) {
       printf("Error opening directory.\n");
       return 1;
     }
-    // First pass: Process Sys.vm
-    char sysVmPath[512];
-    snprintf(sysVmPath, sizeof(sysVmPath), "%s/Sys.vm", argv[1]);
-    processFile(sysVmPath, file);
+
+    //First write the init instruction
+    writeInitAssembly(file);
 
     // Second pass: Process all other .vm files
     while ((entry = readdir(dir)) != NULL) {
       char fileInDir[512];
       snprintf(fileInDir, sizeof(fileInDir), "%s/%s", argv[1], entry->d_name);
-      if (strstr(fileInDir, ".vm") && strcmp(entry->d_name, "Sys.vm")) {
-        processFile(fileInDir, file);
+      if (strstr(fileInDir, ".vm")) {
+        processFile(fileInDir, file, memory);
       }
     }
 
@@ -155,8 +167,7 @@ int main(int argc, char *argv[]) {
     if (!file.file) {
       return 1; // exit if error occurred
     }
-    printf("%s\n", file.filename);
-    processFile(argv[1], file);
+    processFile(argv[1], file, memory);
   } else {
     printf("Unknown file type.\n");
     return 1;
