677f80ff435f762ab3e76288fce5227d65cc2c9a
SFYLL <santiagoflood@hotmail.fr>
2023-10-27 11:20:11 +0200

    update apis

--- projects/10/JackAnalyzer/JackAnalyzer.py
+++ projects/10/JackAnalyzer/JackAnalyzer.py
@@ -2,6 +2,7 @@ import os
 import sys 
 
 from JackTokenizer import JackTokenizer
+from CompilationEngine import CompilationEngine 
 
 class JackAnalyzer:
     def __init__(self, file_location):
@@ -9,39 +10,19 @@ class JackAnalyzer:
         self.output_file = self.get_output_file_location()
         self.tokenizer = JackTokenizer(file_location)
         print(self.tokenizer.tokens)
+        self.compilation_engine = CompilationEngine(self.tokenizer)
+        self.run()
+
+    def run(self) -> None:
+        self.compilation_engine.compileClass()
+        self.write_xml(self.compilation_engine.output) 
 
     def get_output_file_location(self) -> str:
         base_name = os.path.splitext(self.input_file)[0]
         output_file_path = base_name + "est.xml"
         return output_file_path
 
-    def generate_tokens_xml(self) -> str:
-        xml_output = "<tokens>\n"
-
-        while self.tokenizer.hasMoreTokens():
-
-            token_type = self.tokenizer.advance_and_get_token_type()
-            token = self.tokenizer.current_token
-
-            if token_type == "symbol" and token in ['<', '>', '&', '"']:
-                if token == '<':
-                    token = '&lt;'
-                elif token == '>':
-                    token = '&gt;'
-                elif token == '&':
-                    token = '&amp;'
-                elif token == '"':
-                    token = '&quot'
-            if token_type == "stringConstant":
-                token = token[1:-1] 
-
-            xml_output += f"  <{token_type}> {token} </{token_type}>\n"
-
-        xml_output += "</tokens>"
-        return xml_output
-
-    def generate_and_write_xml(self) -> None:
-        xml_content = self.generate_tokens_xml()
+    def write_xml(self, xml_content) -> None:
         with open(self.output_file, "w") as f:
             f.write(xml_content)
 
@@ -55,7 +36,6 @@ if __name__ == "__main__":
     if os.path.isfile(input_path):
         if input_path.endswith('.jack'):
             executor = JackAnalyzer(input_path)
-            executor.generate_and_write_xml()
         else:
             print("Provided file is not a .jack file.")
     elif os.path.isdir(input_path):
@@ -63,7 +43,6 @@ if __name__ == "__main__":
             if file_name.endswith('.jack'):
                 full_file_path = os.path.join(input_path, file_name)
                 executor = JackAnalyzer(full_file_path)
-                executor.generate_and_write_xml()
     else:
         print("Provided path is neither a file nor a directory.") 
     
--- projects/10/JackAnalyzer/JackTokenizer.py
+++ projects/10/JackAnalyzer/JackTokenizer.py
@@ -26,14 +26,26 @@ class JackTokenizer:
         
         return content
 
+    def is_operator(self):
+        operators = ['+', '-', '*', '/', '&', '|', '<', '>', '=']
+        return self.current_token() in operators
+
     def hasMoreTokens(self):
         return self.current_index + 1 < len(self.tokens)
 
+    def hasLessTokens(self):
+        return self.current_index - 1 > len(self.tokens)
+    
     def advance(self):
         if self.hasMoreTokens():
             self.current_index += 1
             self.current_token = self.tokens[self.current_index]
 
+    def retreat(self):
+        if self.hasLessTokens():
+            self.current_index -= 1
+            self.current_token = self.tokens[self.current_index]
+    
     def advance_and_get_token(self) -> Optional[Union[int, str]]:
         self.advance()
         return self.current_token
@@ -41,6 +53,42 @@ class JackTokenizer:
     def advance_and_get_token_type(self) -> Optional[Union[int, str]]:
         self.advance()
         return self.token_type()
+    
+    def peek_next_token(self):
+        self.advance()
+        next_token = self.current_token
+        self.retreat()
+        return next_token
+
+    def expect_symbol(self, symbol = False) -> bool:
+        if not symbol:
+            return self.token_type() == "symbol"
+        else:    
+            return self.token_type() == "symbol" and self.current_token == symbol
+
+    def expect_keyword(self, keyword = False) -> bool:
+        if not keyword:
+            return self.token_type() == "keyword"
+        else:    
+            return self.token_type() == "keyword" and self.current_token == keyword
+    
+    def expect_integerConstant(self, integerConstant = False) -> bool:
+        if not integerConstant:
+            return self.token_type() == "integerConstant"
+        else:    
+            return self.token_type() == "integerConstant" and self.current_token == integerConstant
+
+    def expect_stringConstant(self, stringConstant = False) -> bool:
+        if not stringConstant:
+            return self.token_type() == "stringConstant"
+        else:    
+            return self.token_type() == "stringConstant" and self.current_token == stringConstant
+    
+    def expect_identifier(self, identifier = False) -> bool:
+        if not identifier:
+            return self.token_type() == "identifier"
+        else:    
+            return self.token_type() == "identifier" and self.current_token == identifier
     
     def token_type(self):
         if not self.current_token:
